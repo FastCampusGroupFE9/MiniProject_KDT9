61f511903789c0ca5e180a14ca060e0983fd4e03
LeeYongSoo <sideoff0217@naver.com>
2023-08-11 12:50:25 +0900

    release2

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     alias: [{ find: "@", replacement: `${__dirname}/src` }],
   },
   server: {
-    host: "https://miniproject-team9.p-e.kr/",
+    host: "https://hmteresting.netlify.app/",
     proxy: {
       "/mini": {
         target: "https://miniproject-team9.p-e.kr/",
