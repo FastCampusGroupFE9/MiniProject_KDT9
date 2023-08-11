717118b4c061bca8b252f18a77ec45a7c4ac42d7
Lee Yongsoo <118169266+dragon-it@users.noreply.github.com>
2023-08-11 12:37:07 +0900

    Update apis.ts

--- src/Api/apis.ts
+++ src/Api/apis.ts
@@ -12,20 +12,20 @@ export const getAccessToken = (): string | undefined => {
 
 const ACCESSTOKEN = getAccessToken();
 export const ApiHttp: AxiosInstance = axios.create({
-  baseURL: "/mini",
+  baseURL: "/",
   headers: {
     Authorization: `Bearer ${ACCESSTOKEN}`,
   },
 });
 
 export const ApiLogin: AxiosInstance = axios.create({
-  baseURL: "/mini",
+  baseURL: "/",
 });
 
 // 재요청 인스턴스
 export const getSilentAxios = (token: string): AxiosInstance => {
   const silentAxios: AxiosInstance = axios.create({
-    baseURL: "/mini",
+    baseURL: "/",
     headers: {
       Authorization: `Bearer ${token}`,
     },
