47883ccd861956399977d5fd0cd6152a9395fb1e
Lee Yongsoo <118169266+dragon-it@users.noreply.github.com>
2023-08-11 15:31:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     alias: [{ find: "@", replacement: `${__dirname}/src` }],
   },
   server: {
-    host: "hmteresting.netlify.app",
+    host: "https://hmteresting.netlify.app",
     proxy: {
       "/mini": {
         target: "https://miniproject-team9.p-e.kr/",
