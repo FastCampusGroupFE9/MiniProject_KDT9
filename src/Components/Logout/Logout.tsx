410037db69e3742ad47a6627d174ff914c4f7ed0
Lee Yongsoo <118169266+dragon-it@users.noreply.github.com>
2023-08-16 15:04:18 +0900

    Update Logout.tsx

--- src/Components/Logout/Logout.tsx
+++ src/Components/Logout/Logout.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { logOut } from "../../Api/apis";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 const Logout: React.FC = () => {
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
   const navigate = useNavigate();
   const [, , removeCookies] = useCookies();
 
   const handleLogout = async (): Promise<void> => {
     try {
-
       const res = await logOut();
       if (res) {
         localStorage.removeItem("role");
         removeCookies("accessToken");
+        setShowConfirmModal(false);
 
         alert("로그아웃 되었습니다.");
         navigate("/");
@@ -24,9 +25,29 @@ const Logout: React.FC = () => {
     }
   };
 
+  const handleClickLogout = () => {
+    setShowConfirmModal(true);
+  };
+
+  const handleConfirmLogout = () => {
+    setShowConfirmModal(false);
+    handleLogout();
+  };
+
+  const handleCancelLogout = () => {
+    setShowConfirmModal(false);
+  };
+
   return (
     <>
-      <li onClick={handleLogout}>로그아웃</li>
+      <li onClick={handleClickLogout}>로그아웃</li>
+      {showConfirmModal && (
+        <div className="logout-confirm-modal">
+          <p>이미 로그인 되어있습니다. 로그아웃 하시겠습니까?</p>
+          <button onClick={handleConfirmLogout}>예</button>
+          <button onClick={handleCancelLogout}>아니오</button>
+        </div>
+      )}
     </>
   );
 };
